87d51c5b80030007bc26be52304e1a0ccff5660c
veerapat <veeraput.kb09834@gmail.com>
2025-05-31 11:03:21 +0700

    refactor: update page component structure and improve formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { motion } from "framer-motion";
-
+import { Badge } from "@/components/ui/badge";
 import {
   Card,
   CardContent,
@@ -9,132 +7,115 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Badge } from "@/components/ui/badge";
-import {
-  ArrowRight,
-  CheckCircle,
-  Zap,
-  Shield,
-  Users,
-  Star,
-  Server,
-} from "lucide-react";
-import Link from "next/link";
-{
-}
+import { ArrowRight, CheckCircle, Zap, Shield, Server } from "lucide-react";
+
 export default function Page() {
   return (
-    <div className="flex flex-col min-h-screen items-center">
-      <main className="flex-1">
-        <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 bg-gradient-to-br from-primary/10 via-background to-secondary/10">
-          <div className="container px-4 md:px-6">
-            <div className="flex flex-col items-center space-y-4 text-center align-middle justify-self-center">
-              <div className="space-y-2">
-                <Badge variant="secondary" className="mb-4">
-                  🚀 Get Started Today
-                </Badge>
-                <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-                  Get updated with real time air quality
-                </h1>
-                <p className="mx-auto max-w-[700px] text-muted-foreground md:text-xl">
-                  Join us a real time air quality data is waiting for you.
-                </p>
-              </div>
-              <div className="space-x-4">
-                <Button size="lg" className="h-12 px-8">
-                  Get Started Free
-                  <ArrowRight className="ml-2 h-4 w-4" />
-                </Button>
-              </div>
-            </div>
-          </div>
-        </section>
-        <br />
+    <div className="h-screen snap-y snap-mandatory overflow-y-scroll scroll-smooth bg-orange-300">
+      <section className="min-h-screen snap-center flex flex-col justify-center items-center bg-gradient-to-br from-primary/10 via-background to-secondary/10 px-4 md:px-6 text-center">
+        <Badge variant="secondary" className="mb-4">
+          🚀 Get Started Today
+        </Badge>
+        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-tighter mb-4">
+          Get updated with real time air quality
+        </h1>
+        <p className="max-w-[700px] text-muted-foreground md:text-xl mb-6">
+          Join us, real-time air quality data is waiting for you.
+        </p>
+        <div className="flex flex-col md:flex-row gap-4 ">
+          <a href="/login">
+            <Button size="lg">Get Started Free</Button>
+          </a>
+          <a href="#features">
+            <Button size="lg">Learn more</Button>
+          </a>
+        </div>
+      </section>
 
-        <section id="features" className="w-auto py-12 md:py-11 lg:py-32">
-          <div className="container px-4 md:px-6">
-            <div className="mx-auto grid max-w-5xl items-start gap-6 py-12 lg:grid-cols-3 lg:gap-12">
-              <Card>
-                <CardHeader>
-                  <Zap className="h-10 w-10 text-primary" />
-                  <CardTitle>Fast Realtime Data</CardTitle>
-                  <CardDescription>
-                    Get update with real time air quality data from sensors.
-                  </CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <ul className="space-y-2 text-sm">
-                    <li className="flex items-center">
-                      <CheckCircle className="h-4 w-4 text-green-500 mr-2" />
-                      Real time database
-                    </li>
-                    <li className="flex items-center">
-                      <CheckCircle className="h-4 w-4 text-green-500 mr-2" />
-                      Accurate
-                    </li>
-                    <li className="flex items-center">
-                      <CheckCircle className="h-4 w-4 text-green-500 mr-2" />
-                      Low latency
-                    </li>
-                  </ul>
-                </CardContent>
-              </Card>
-              <Card>
-                <CardHeader>
-                  <Server className="h-10 w-10 text-primary" />
-                  <CardTitle>Scalable Infrastructure</CardTitle>
-                  <CardDescription>
-                    Our platform is built on a scalable infrastructure to handle
-                    large amounts of data and users.
-                  </CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <ul className="space-y-2 text-sm">
-                    <li className="flex items-center">
-                      <CheckCircle className="h-4 w-4 text-green-500 mr-2" />
-                      Fast data processing
-                    </li>
-                    <li className="flex items-center">
-                      <CheckCircle className="h-4 w-4 text-green-500 mr-2" />
-                      Fast data storage
-                    </li>
-                    <li className="flex items-center">
-                      <CheckCircle className="h-4 w-4 text-green-500 mr-2" />
-                      Fast network system
-                    </li>
-                    <li className="flex items-center">
-                      <CheckCircle className="h-4 w-4 text-green-500 mr-2" />
-                      Backup
-                    </li>
-                    <li className="flex items-center">
-                      <CheckCircle className="h-4 w-4 text-green-500 mr-2" />
-                      Low latency
-                    </li>
-                  </ul>
-                </CardContent>
-              </Card>
-              <Card>
-                <CardHeader>
-                  <Shield className="h-10 w-10 text-primary" />
-                  <CardTitle>Secure & Reliable</CardTitle>
-                  <CardDescription>
-                    Your data is safe with us. We prioritize security and
-                    reliability in our platform.
-                  </CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <ul className="space-y-2 text-sm">
-                    <li className="flex items-center">
-                      <CheckCircle className="h-4 w-4 text-green-500 mr-2" />
-                      End-to-end encryption
-                    </li>
-                  </ul>
-                </CardContent>
-              </Card>
-            </div>
-          </div>
-        </section>
-      </main>
+      <section
+        id="features"
+        className="min-h-screen snap-center py-16 px-4 md:px-6 bg-white"
+      >
+        <div className="mx-auto grid max-w-5xl items-start gap-6 lg:grid-cols-3 lg:gap-12">
+          <Card>
+            <CardHeader>
+              <Zap className="h-10 w-10 text-primary" />
+              <CardTitle>Fast Realtime Data</CardTitle>
+              <CardDescription>
+                Get update with real time air quality data from sensors.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <ul className="space-y-2 text-sm">
+                <li className="flex items-center">
+                  <CheckCircle className="h-4 w-4 text-green-500 mr-2" />
+                  Real time database
+                </li>
+                <li className="flex items-center">
+                  <CheckCircle className="h-4 w-4 text-green-500 mr-2" />
+                  Accurate
+                </li>
+                <li className="flex items-center">
+                  <CheckCircle className="h-4 w-4 text-green-500 mr-2" />
+                  Low latency
+                </li>
+              </ul>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader>
+              <Server className="h-10 w-10 text-primary" />
+              <CardTitle>Scalable Infrastructure</CardTitle>
+              <CardDescription>
+                Our platform is built on a scalable infrastructure to handle
+                large amounts of data and users.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <ul className="space-y-2 text-sm">
+                <li className="flex items-center">
+                  <CheckCircle className="h-4 w-4 text-green-500 mr-2" />
+                  Fast data processing
+                </li>
+                <li className="flex items-center">
+                  <CheckCircle className="h-4 w-4 text-green-500 mr-2" />
+                  Fast data storage
+                </li>
+                <li className="flex items-center">
+                  <CheckCircle className="h-4 w-4 text-green-500 mr-2" />
+                  Fast network system
+                </li>
+                <li className="flex items-center">
+                  <CheckCircle className="h-4 w-4 text-green-500 mr-2" />
+                  Backup
+                </li>
+                <li className="flex items-center">
+                  <CheckCircle className="h-4 w-4 text-green-500 mr-2" />
+                  Low latency
+                </li>
+              </ul>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader>
+              <Shield className="h-10 w-10 text-primary" />
+              <CardTitle>Secure & Reliable</CardTitle>
+              <CardDescription>
+                Your data is safe with us. We prioritize security and
+                reliability in our platform.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <ul className="space-y-2 text-sm">
+                <li className="flex items-center">
+                  <CheckCircle className="h-4 w-4 text-green-500 mr-2" />
+                  End-to-end encryption
+                </li>
+              </ul>
+            </CardContent>
+          </Card>
+        </div>
+      </section>
     </div>
   );
 }
